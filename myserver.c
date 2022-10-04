ece5b7bff8cdf228dfd481da05548f7476880377
= <=>
2022-10-04 01:54:10 -0500

    Create my server

--- myserver.c
+++ myserver.c
@@ -13,11 +13,57 @@
 #include <stdio.h>
 #include <string.h>
 
+/* multithread */
+#include <pthread.h>
+
+
 /* server parameters */
 #define SERV_PORT 8080                 /* port */
-#define SERV_HOST_ADDR "172.31.17.199" /* IP, only IPV4 support  */
+#define SERV_HOST_ADDR "192.168.0.21" /* IP, only IPV4 support  */
 #define BUF_SIZE 100                   /* Buffer rx, tx max size  */
 #define BACKLOG 5                      /* Max. client pending connections  */
+
+struct thread_connection{
+    pthread_t thid;
+    int connection_id;
+};
+
+void* fun(void * params){
+    struct thread_connection connfd_aux = *(struct thread_connection*)params;
+    char *reply = 
+		"HTTP/1.1 200 OK\n"
+		"Date: Thu, 19 Feb 2009 12:27:04 GMT\n"
+		"Server: Apache/2.2.3\n"
+		"Last-Modified: Wed, 18 Jun 2003 16:05:58 GMT\n"
+		"ETag: \"56d-9989200-1132c580\"\n"
+		"Content-Type: text/html\n"
+		"Content-Length: 15\n"
+		"Accept-Ranges: bytes\n"
+		"Connection: close\n"
+		"\n"
+		"sdfkjsdnbfkjbsf";
+        
+    while(1) {  
+                char buff_rx[BUF_SIZE];  
+                /* read client message, copy it into buffer */
+                int len_rx = read(connfd_aux.connection_id, buff_rx, sizeof(buff_rx));
+                if(len_rx == -1){
+                    printf("[SERVER]: ERROR READING CLIENT CONTENT");
+                }
+                /* if length is 0 client socket closed, then exit */
+                else if(len_rx == 0) {
+                    printf("[SERVER]: client socket closed \n\n");
+                    close(connfd_aux.connection_id);
+                    break; 
+                }else{
+                    send(connfd_aux.connection_id,reply, strlen(reply),0);
+                    printf("%s", buff_rx);
+                }            
+    }  
+    //pthread_exit(&connfd_aux.connection_id);
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -31,17 +77,49 @@ int main(int argc, char *argv[])
     // Create socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd == -1){
-        printf("[SERVER]: ERROR CREATING SOCKET")
+        printf("[SERVER]: ERROR CREATING SOCKET\n");
+        return -1;
     }else{
-        printf("[SERVER]: Socket created successful")
+        printf("[SERVER]: Socket created successful\n");
     }
 
     // Bind
-    int socket_bind = bind(socket, (struct sockaddr *)sock_address, sizeof(sock_address));
+    int socket_bind = bind(sockfd, (struct sockaddr *)&sock_address, sizeof(sock_address));
     if(socket_bind != 0){
-        printf("[SERVER]: ERROR BIND SOCKET")
+        printf("[SERVER]: ERROR SOCKET BIND");
+        return -1;
+        return -1;
     }else{
-        printf("[SERVER]: Socket bind 
-        excecuted successful")
+        printf("[SERVER]: Socket bind excecuted successful\n");
     }
+
+    //Listen
+    int socket_listen = listen(sockfd,BACKLOG);
+    if(socket_listen != 0){
+        printf("[SERVER]: ERROR LISTENING\n");
+        return -1;
+    }else{
+        printf("[SERVER]: SOCKET LISTENING\n");
+    }
+
+    //Conections
+    while(1){
+        struct sockaddr_in client;
+        int len_client = sizeof(client);
+        int connfd;
+
+        //Accept connection
+        connfd = accept(sockfd, (struct sockaddr *)&client, &len_client); 
+        if(connfd<0){
+            printf("[SERVER]: ERROR TO CONNECT WITH CLIENT");
+            return -1;
+        }else{
+            pthread_t tid0;
+            struct thread_connection thr_conn;
+            thr_conn.thid = tid0;
+            thr_conn.connection_id = connfd;
+            pthread_create(&tid0,NULL,fun,&thr_conn);
+        }
+    }
+
 }
